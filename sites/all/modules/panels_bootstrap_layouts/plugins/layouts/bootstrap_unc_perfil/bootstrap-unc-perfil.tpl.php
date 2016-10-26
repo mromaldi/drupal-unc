15207fb8ca5aa10048599535b51e72b8bddf3611
Mariana Romaldi <mariana.romaldi@unc.edu.ar>
2016-10-26 13:19:49 -0300

    agregnado layout para perfil

---
+++ sites/all/modules/panels_bootstrap_layouts/plugins/layouts/bootstrap_unc_perfil/bootstrap-unc-perfil.tpl.php
@@ -0,0 +1,37 @@
+<div class="<?php print $classes ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+  <?php if ($content['slider_noticias']): ?>
+    <div class="row">
+      <div class="col-xs-12 col-md-12">
+        <?php print $content['slider_noticias']; ?>
+      </div>
+    </div>
+  <?php endif ?>
+  <?php if ($content['cintas_desplegables']): ?>
+    <div class="row">
+      <div class="col-xs-12 col-md-12">
+        <?php print $content['cintas_desplegables']; ?>
+      </div>
+    </div>
+  <?php endif ?>
+
+  <?php if ($content['novedades'] || $content['videos']): ?>
+    <div class="row">
+      <div class="col-xs-12 col-md-6">
+        <?php print $content['novedades']; ?>
+      </div>
+      <div class="col-xs-12 col-md-6">
+        <?php print $content['videos']; ?>
+      </div> 
+    </div>
+  <?php endif ?>
+
+
+  <?php if ($content['tira_banners']): ?>
+    <div class="row">
+      <div class="col-xs-12 col-md-12">
+        <?php print $content['tira_banners']; ?>
+      </div>
+    </div>
+  <?php endif ?>
+
+</div>
